1c288308ac9cb15704e3f8be365c11b422a48b62
mohmed <mohmedamged9@gmail.com>
2020-09-12 20:28:35 +0200

    added the logic

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="cal">
     <div id="cal_container">
-      <div class="display">0</div>
-      <div class="grid_item gray">C</div>
-      <div class="grid_item gray">+/-</div>
-      <div class="grid_item gray">%</div>
-      <div class="grid_item orange">/</div>
-      <div class="grid_item gray">7</div>
-      <div class="grid_item gray">8</div>
-      <div class="grid_item gray">9</div>
-      <div class="grid_item orange">*</div>
-      <div class="grid_item gray">4</div>
-      <div class="grid_item gray">5</div>
-      <div class="grid_item gray">6</div>
-      <div class="grid_item orange">-</div>
-      <div class="grid_item gray">1</div>
-      <div class="grid_item gray">2</div>
-      <div class="grid_item gray">3</div>
-      <div class="grid_item orange">+</div>
-      <div class="grid_item gray zero">0</div>
-      <div class="grid_item gray">.</div>
-      <div class="grid_item gray">=</div>
+      <div class="display">{{ display || "0" }}</div>
+      <div @click="clear" class="grid_item gray">C</div>
+      <div @click="nagv" class="grid_item gray">+/-</div>
+      <div @click="precent" class="grid_item gray">%</div>
+      <div @click="div" class="grid_item orange">/</div>
+      <div @click="append('7')" class="grid_item gray">7</div>
+      <div @click="append('8')" class="grid_item gray">8</div>
+      <div @click="append('9')" class="grid_item gray">9</div>
+      <div @click="mul" class="grid_item orange">*</div>
+      <div @click="append('4')" class="grid_item gray">4</div>
+      <div @click="append('5')" class="grid_item gray">5</div>
+      <div @click="append('6')" class="grid_item gray">6</div>
+      <div @click="sub" class="grid_item orange">-</div>
+      <div @click="append('1')" class="grid_item gray">1</div>
+      <div @click="append('2')" class="grid_item gray">2</div>
+      <div @click="append('3')" class="grid_item gray">3</div>
+      <div @click="add" class="grid_item orange">+</div>
+      <div @click="append('0')" class="grid_item gray zero">0</div>
+      <div @click="dot" class="grid_item gray">.</div>
+      <div @click="equal" class="grid_item gray">=</div>
     </div>
   </div>
 </template>
@@ -28,6 +28,84 @@
 <script>
 export default {
   name: "calculator",
+  data() {
+    return {
+      prev: "",
+      display: "",
+      opClicked: false,
+      op: null,
+    };
+  },
+  methods: {
+    clear() {
+      this.display = "";
+    },
+    nagv() {
+      console.log(this.display.charAt(0));
+      this.display =
+        this.display.charAt(0) === "-"
+          ? this.display.slice(1)
+          : `-${this.display}`;
+    },
+    precent() {
+      if (this.display !== "0") {
+        this.display = `${parseFloat(this.display) / 100}`;
+      }
+    },
+    append(num) {
+      if (this.opClicked) {
+        this.display = "";
+        this.opClicked = false;
+      }
+      if (this.display !== "" || num !== "0") {
+        if (this.display === "0") {
+          this.display = num;
+        } else {
+          this.display = `${this.display}${num}`;
+        }
+      }
+    },
+    dot() {
+      if (this.display.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    setPrev() {
+      this.prev = this.display;
+      this.opClicked = true;
+    },
+    add() {
+      this.op = (a, b) => {
+        return a + b;
+      };
+      this.setPrev();
+    },
+    div() {
+      this.op = (a, b) => {
+        return a / b;
+      };
+      this.setPrev();
+    },
+    mul() {
+      this.op = (a, b) => {
+        return a * b;
+      };
+      this.setPrev();
+    },
+    sub() {
+      this.op = (a, b) => {
+        return a - b;
+      };
+      this.setPrev();
+    },
+    equal() {
+      this.display = `${this.op(
+        parseFloat(this.prev),
+        parseFloat(this.display)
+      )}`;
+      this.prev = "";
+    },
+  },
 };
 </script>
 
